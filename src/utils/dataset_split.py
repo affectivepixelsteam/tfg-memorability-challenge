969fe783de068420541070cef229edc637f8c85d
Marcos <marquitos.mcm@gmail.com>
2019-11-25 13:30:20 +0100

    Fixed train and test split for whole dataset and splitted dataset
    First we split the whole dataset in train and test sets. Then with each train and test sets we split them getting the 25% higher memorable videos and the 25% lower memorable videos

--- src/utils/dataset_split.py
+++ src/utils/dataset_split.py
@@ -1,30 +1,59 @@
 import pandas as pd
 import numpy as np
 
-# Path to files/folders
-ground_truth_file_path = '../../data/corpus/devset/dev-set/ground-truth/ground-truth_dev-set.csv'
-captions_file_path = '../../data/corpus/devset/dev-set/dev-set_video-captions-cleanup.csv'
+# Ground truth input file paths
+train_ground_truth_file_path = '../../data/corpus/devset/dev-set/ground-truth/train_ground-truth_dev-set.csv'
+test_ground_truth_file_path = '../../data/corpus/devset/dev-set/ground-truth/test_ground-truth_dev-set.csv'
 
-splitted_ground_truth_file_path = '../../data/corpus/devset/dev-set/ground-truth/ground-truth_dev-set_splitted.csv'
-splitted_captions_file_path = '../../data/corpus/devset/dev-set/dev-set_video-captions-cleanup_splitted.csv'
+# Ground truth output file paths
+train_ground_truth_splitted_file_path = '../../data/corpus/devset/dev-set/ground-truth/train_ground-truth_dev-set_splitted.csv'
+test_ground_truth_splitted_file_path = '../../data/corpus/devset/dev-set/ground-truth/test_ground-truth_dev-set_splitted.csv'
+
+# Captions input file paths
+train_captions_file_path = '../../data/corpus/devset/dev-set/train_dev-set_video-captions-cleanup.csv'
+test_captions_file_path = '../../data/corpus/devset/dev-set/test_dev-set_video-captions-cleanup.csv'
+
+# Captions output file paths
+train_captions_splitted_file_path = '../../data/corpus/devset/dev-set/train_dev-set_video-captions-cleanup_splitted.csv'
+test_captions_splitted_file_path = '../../data/corpus/devset/dev-set/test_dev-set_video-captions-cleanup_splitted.csv'
 
 # load dataframes
-df_ground_truth = pd.read_csv(ground_truth_file_path)
-df_captions = pd.read_csv(captions_file_path)
+df_train_ground_truth = pd.read_csv(train_ground_truth_file_path)
+df_test_ground_truth = pd.read_csv(test_ground_truth_file_path)
+
+df_train_captions = pd.read_csv(train_captions_file_path)
+df_test_captions = pd.read_csv(test_captions_file_path)
 
 # order columns by long-term_memorability
 # We have chosen long term memorability because it has a lower mean and higher variance
-df_ground_truth = df_ground_truth.sort_values(by=['long-term_memorability'], ascending=False)
+df_train_ground_truth = df_train_ground_truth.sort_values(by=['long-term_memorability'], ascending=False)
+df_test_ground_truth = df_test_ground_truth.sort_values(by=['long-term_memorability'], ascending=False)
 
 # Now that it is ordered, split it.
-higher_values, middle_values, lower_values = np.split(df_ground_truth, [int(0.25*len(df_ground_truth)), int(0.75*len(df_ground_truth))])
+train_higher_values, train_middle_values, train_lower_values = np.split(df_train_ground_truth, [int(0.25*len(df_train_ground_truth)), int(0.75*len(df_train_ground_truth))])
+test_higher_values, test_middle_values, test_lower_values = np.split(df_test_ground_truth, [int(0.25*len(df_test_ground_truth)), int(0.75*len(df_test_ground_truth))])
 
 # Merge higher values and lower values
-df_ground_truth_splitted = pd.concat([higher_values, lower_values])
+df_train_ground_truth_splitted = pd.concat([train_higher_values, train_lower_values])
+df_test_ground_truth_splitted = pd.concat([test_higher_values, test_lower_values])
 
 # Now split the captions dataset too.
-df_captions_splitted = df_captions.iloc[df_ground_truth_splitted.index.values.tolist(),:]
+df_train_captions_splitted = df_train_captions[df_train_captions.id.isin(df_train_ground_truth_splitted.video)]
+df_test_captions_splitted = df_test_captions[df_test_captions.id.isin(df_test_ground_truth_splitted.video)]
+
+# Sort them
+df_train_ground_truth_splitted = df_train_ground_truth_splitted.sort_values(by=['video'])
+df_test_ground_truth_splitted = df_test_ground_truth_splitted.sort_values(by=['video'])
+
+df_train_captions_splitted = df_train_captions_splitted.sort_values(by=['id'])
+df_test_captions_splitted = df_test_captions_splitted.sort_values(by=['id'])
 
 # Finally, save the new datasets
-df_ground_truth_splitted.to_csv(splitted_ground_truth_file_path, index=False)
-df_captions_splitted.to_csv(splitted_captions_file_path, index=False)
+print ('Ground truth splitted set [train=' + str(len(df_train_ground_truth_splitted)) + ",test=" + str(len(df_test_ground_truth_splitted)) + "]")
+print ('Captions splitted set [train=' + str(len(df_train_captions_splitted)) + ",test=" + str(len(df_test_captions_splitted)) + "]")
+
+df_train_ground_truth_splitted.to_csv(train_ground_truth_splitted_file_path, index=False)
+df_test_ground_truth_splitted.to_csv(test_ground_truth_splitted_file_path, index=False)
+
+df_train_captions_splitted.to_csv(train_captions_splitted_file_path, index=False)
+df_test_captions_splitted.to_csv(test_captions_splitted_file_path, index=False)
--- src/utils/train_test_split.py
+++ src/utils/train_test_split.py
@@ -5,10 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split 
 
 # So we are going to get the train and test set from the splitted dataset and the whole dataset.
-ground_truth_splitted_file_path = '../../data/corpus/devset/dev-set/ground-truth/ground-truth_dev-set_splitted.csv'
 ground_truth_file_path = '../../data/corpus/devset/dev-set/ground-truth/ground-truth_dev-set.csv'
-
-captions_splitted_file_path = '../../data/corpus/devset/dev-set/dev-set_video-captions-cleanup_splitted.csv'
 captions_file_path = '../../data/corpus/devset/dev-set/dev-set_video-captions-cleanup.csv'
 
 # Split params
@@ -17,57 +14,39 @@ round_state = 123
 
 
 # Ground truth output file paths
-train_ground_truth_splitted_file_path = '../../data/corpus/devset/dev-set/ground-truth/train_ground-truth_dev-set_splitted.csv'
-test_ground_truth_splitted_file_path = '../../data/corpus/devset/dev-set/ground-truth/test_ground-truth_dev-set_splitted.csv'
 train_ground_truth_file_path = '../../data/corpus/devset/dev-set/ground-truth/train_ground-truth_dev-set.csv'
 test_ground_truth_file_path = '../../data/corpus/devset/dev-set/ground-truth/test_ground-truth_dev-set.csv'
 
 # Captions output file paths
-train_captions_splitted_file_path = '../../data/corpus/devset/dev-set/train_dev-set_video-captions-cleanup_splitted.csv'
-test_captions_splitted_file_path = '../../data/corpus/devset/dev-set/test_dev-set_video-captions-cleanup_splitted.csv'
 train_captions_file_path = '../../data/corpus/devset/dev-set/train_dev-set_video-captions-cleanup.csv'
 test_captions_file_path = '../../data/corpus/devset/dev-set/test_dev-set_video-captions-cleanup.csv'
 
 
 # Get dataframes.
-df_ground_truth_splitted = pd.read_csv(ground_truth_splitted_file_path)
-df_ground_truth_all = pd.read_csv(ground_truth_file_path)
-
-df_captions_splitted = pd.read_csv(captions_splitted_file_path)
-df_captions_all = pd.read_csv(captions_file_path)
-
+df_ground_truth = pd.read_csv(ground_truth_file_path)
+df_captions = pd.read_csv(captions_file_path)
 
 # Split ground_truths
-train_ground_truth_splitted, test_ground_truth_splitted = train_test_split(df_ground_truth_splitted, test_size=test_size, random_state=round_state)
-train_ground_truth_all, test_ground_truth_all = train_test_split(df_ground_truth_all, test_size=test_size, random_state=round_state)
+train_ground_truth_all, test_ground_truth_all = train_test_split(df_ground_truth, test_size=test_size, random_state=round_state)
 
 # Do the same split on captions files
-train_captions_splitted = df_captions_splitted[df_captions_splitted.id.isin(train_ground_truth_splitted.video)]
-test_captions_splitted = df_captions_splitted[df_captions_splitted.id.isin(test_ground_truth_splitted.video)]
-
-train_captions_all = df_captions_all[df_captions_all.id.isin(train_ground_truth_all.video)]
-test_captions_all = df_captions_all[df_captions_all.id.isin(test_ground_truth_all.video)]
+train_captions_all = df_captions[df_captions.id.isin(train_ground_truth_all.video)]
+test_captions_all = df_captions[df_captions.id.isin(test_ground_truth_all.video)]
 
 # Sort values before saving them.
-train_ground_truth_splitted = train_ground_truth_splitted.sort_values(by=['video'])
-test_ground_truth_splitted = test_ground_truth_splitted.sort_values(by=['video'])
-
 train_ground_truth_all = train_ground_truth_all.sort_values(by=['video'])
 test_ground_truth_all = test_ground_truth_all.sort_values(by=['video'])
-
-train_captions_splitted = train_captions_splitted.sort_values(by=['id'])
-test_captions_splitted = test_captions_splitted.sort_values(by=['id'])
 
 train_captions_all = train_captions_all.sort_values(by=['id'])
 test_captions_all = test_captions_all.sort_values(by=['id'])
 
 # Save files
-train_ground_truth_splitted.to_csv(train_ground_truth_splitted_file_path, index=False)
-test_ground_truth_splitted.to_csv(test_ground_truth_splitted_file_path, index=False)
+
+print ('Ground truth set [train=' + str(len(train_ground_truth_all)) + ",test=" + str(len(test_ground_truth_all)) + "]")
+print ('Captions set [train=' + str(len(train_captions_all)) + ",test=" + str(len(test_captions_all)) + "]")
+
 train_ground_truth_all.to_csv(train_ground_truth_file_path, index=False)
 test_ground_truth_all.to_csv(test_ground_truth_file_path, index=False)
 
-train_captions_splitted.to_csv(train_captions_splitted_file_path, index=False)
-test_captions_splitted.to_csv(test_captions_splitted_file_path, index=False)
 train_captions_all.to_csv(train_captions_file_path, index=False)
 test_captions_all.to_csv(test_captions_file_path, index=False)
