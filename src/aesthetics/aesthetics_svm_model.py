f33094537f9cb0fc0c71259eaf221112fb358462
Marcos Collado <marquitos.mcm@gmail.com>
2020-01-05 17:53:01 +0100

    SVM models: Train for both datasets and labels (short and long)

--- src/aesthetics/aesthetics_svm_model.py
+++ src/aesthetics/aesthetics_svm_model.py
@@ -5,52 +5,55 @@ import xgboost as xgb
 from sklearn import svm
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import mean_squared_error
-from scipy.stats import spearmanr
+from scipy.stats import spearmanr, pearsonr
 from scipy.stats import uniform, randint
 
-use_splitted_db = False
-use_short_term = False
-RANDOM_STATE = 44
+for db_type in  [True, False]:
+    for label_type in ['short_term', 'long_term']:
+        use_splitted_db = db_type
+        label = label_type
+        RANDOM_STATE = 44
 
-def report_best_scores(results, n_top=3):
-    for i in range(1, n_top + 1):
-        candidates = np.flatnonzero(results['rank_test_score'] == i)
-        for candidate in candidates:
-            print("Model with rank: {0}".format(i))
-            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
-                  results['mean_test_score'][candidate],
-                  results['std_test_score'][candidate]))
-            print("Parameters: {0}".format(results['params'][candidate]))
-            print("")
+        # Path to file
+        if use_splitted_db:
+            train_embeddings_file_path = '../../data/corpus/devset/dev-set/train_aesthetics_embeddings_splitted_annotations.csv'
+            test_embeddings_file_path = '../../data/corpus/devset/dev-set/test_aesthetics_embeddings_splitted_annotations.csv'
+        else:
+            train_embeddings_file_path = '../../data/corpus/devset/dev-set/train_aesthetics_embeddings_annotations.csv'
+            test_embeddings_file_path = '../../data/corpus/devset/dev-set/test_aesthetics_embeddings_annotations.csv'
 
-# Path to file
-if use_splitted_db:
-    train_embeddings_file_path = '../../data/corpus/devset/dev-set/train_aesthetics_embeddings_splitted_annotations.csv'
-    test_embeddings_file_path = '../../data/corpus/devset/dev-set/test_aesthetics_embeddings_splitted_annotations.csv'
-else:
-    train_embeddings_file_path = '../../data/corpus/devset/dev-set/train_aesthetics_embeddings_annotations.csv'
-    test_embeddings_file_path = '../../data/corpus/devset/dev-set/test_aesthetics_embeddings_annotations.csv'
+        df_train_embeddings = pd.read_csv(train_embeddings_file_path)
+        df_test_embeddings = pd.read_csv(test_embeddings_file_path)
 
-df_train_embeddings = pd.read_csv(train_embeddings_file_path)
-df_test_embeddings = pd.read_csv(test_embeddings_file_path)
+        X_train = df_train_embeddings.iloc[:,3:87].to_numpy()
+        X_test = df_test_embeddings.iloc[:,3:87].to_numpy()
 
-X_train = df_train_embeddings.iloc[:,3:87].to_numpy()
-X_test = df_test_embeddings.iloc[:,3:87].to_numpy()
+        y_train = df_train_embeddings[label].to_numpy()
+        y_test = df_test_embeddings[label].to_numpy()
 
-if use_short_term:
-    y_train = df_train_embeddings['short_term'].to_numpy()
-    y_test = df_test_embeddings['short_term'].to_numpy()
-else:
-    y_train = df_train_embeddings['long_term'].to_numpy()
-    y_test = df_test_embeddings['long_term'].to_numpy()
+        clf = svm.LinearSVR(random_state=RANDOM_STATE, max_iter=4000)
+        clf.fit(X_train, y_train)
 
-clf = svm.LinearSVR(random_state=RANDOM_STATE)
-clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
 
-y_pred = clf.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        spearman = spearmanr(y_test, y_pred)
+        pearson = pearsonr(y_test, y_pred)
 
-mse = mean_squared_error(y_test, y_pred)
-spearman = spearmanr(y_test, y_pred)
+        print ('')
+        print ('')
+        print ('DB ' + str(use_splitted_db) + ' Label ' + str(label))
 
-print ('SVR MSE ' + str(mse))
-print ('SVR ' + str(spearman))
+        print ('SVR MSE ' + str(mse))
+        print ('SVR Spearman' + str(spearman))
+        print ('SVR Pearson' + str(pearson))
+
+        df_test_embeddings["y_pred"] = y_pred
+        avg_test = df_test_embeddings.groupby(["0"]).mean()
+        mse = mean_squared_error(avg_test[label], avg_test["y_pred"])
+        spearman = spearmanr(avg_test[label], avg_test["y_pred"])
+        pearson = pearsonr(avg_test[label], avg_test["y_pred"])
+
+        print('SVR MSE VIDEO ' + str(mse))
+        print('SVR VIDEO ' + str(spearman))
+        print('SVR VIDEO pearson' + str(pearson))
